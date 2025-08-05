e794fee133b9cea593eb3bc280ad459e61741f21
Veera8904 <148971327+Veera8904@users.noreply.github.com>
2025-08-05 10:48:05 +0530

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Linear Search
 def linear_search(arr, target):
     for i in range(len(arr)):
@@ -47,64 +46,3 @@ else:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-#Linear Search
-def linear_search(arr, target):
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i  # Return index if found
-    return -1  # Return -1 if not found
-
-# Example
-arr = [23, 5, 76, 12, 8, 90]
-target = 12
-result = linear_search(arr, target)
-
-if result != -1:
-    print(f"Linear Search: Element found at index {result}")
-else:
-    print("Linear Search: Element not found")
-
-
-
-#Binary Search
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1
-
-# Example
-arr = [5, 8, 12, 23, 76, 90]  # Sorted list
-target = 12
-result = binary_search(arr, target)
-
-if result != -1:
-    print(f"Binary Search: Element found at index {result}")
-else:
-    print("Binary Search: Element not found")
-
->>>>>>> 81a68c1fa28d443c438bfbed79fa006cf1e6973e
